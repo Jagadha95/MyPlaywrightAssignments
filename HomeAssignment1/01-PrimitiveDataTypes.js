285f80e48b9012a5ab2a2e7c1c40be93a39d2356
Jagadha95 <jagadhamurali@gmail.com>
2025-12-20 17:19:00 +0530

    Add files via upload
    Uploaded home assignment 1 - JS

---
+++ HomeAssignment1/01-PrimitiveDataTypes.js
@@ -0,0 +1,7 @@
+let firstName = "John"
+let companyName = 'TestLeaf'
+let mobileNumber = 9086543216
+let isAutomation = true
+let hasPlaywright
+console.log("Values of the variables\nFirst Name: "+ firstName+"\nCompany Name: "+companyName+"\nMobileNumber: "+mobileNumber+"\nIs Automation: "+isAutomation+"\nHas Playwright: "+hasPlaywright)
+console.log("\nDataType of the variables\nFirst Name: "+ typeof(firstName)+"\nCompany Name: "+typeof(companyName)+"\nMobileNumber: "+typeof(mobileNumber)+"\nIs Automation: "+typeof(isAutomation)+"\nHas Playwright: "+typeof(hasPlaywright))
