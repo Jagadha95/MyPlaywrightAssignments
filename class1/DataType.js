9302f9ce652a13e22742fe8dca619ad2be80dea4
Jagadha95 <jagadhamurali@gmail.com>
2025-12-21 22:25:59 +0530

    Delete class1 directory

--- class1/DataType.js
+++
@@ -1,8 +0,0 @@
-var browserVersion = 142.0
-var browserName = "MS Edge"
-var isBrowserOpen = true
-var history = null
-var user
-console.log("Browser Name is "+ browserName + " and its version is "+ browserVersion +"\n"+"Is Browser Opened : "+ isBrowserOpen+" and the history of the browser is "+history)
-console.log("DataTypes of the variables:\nBrowser Name: "+typeof(browserName)+"\nBrowserVersion: "+typeof(browserVersion)+"\nisBrowserOpen: "+typeof(isBrowserOpen)+"\nHistory: "+typeof(history))
-console.log(typeof(user))
