b724e6d5677c839f76f83128680c736373b7cc1d
Jagadha95 <jagadhamurali@gmail.com>
2025-12-21 22:25:01 +0530

    Add files via upload
    Class & Home Assignments as on 06/12/2025 & 07/12/2025

---
+++ week1/HomeAssignment2/01-Odd-Even.js
@@ -0,0 +1,10 @@
+let input = 45
+let isOddOrEven=(input)=>{
+    if(input%2==0){
+        console.log(input +" is an even Number")
+    }
+    else{
+        console.log(input +" is odd Number")
+    }
+}
+isOddOrEven(input)
