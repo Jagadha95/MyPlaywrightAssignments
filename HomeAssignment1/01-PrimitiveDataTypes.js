d4778bd6dccc56fe37509ea280b3697a4673d2f6
Jagadha95 <jagadhamurali@gmail.com>
2025-12-21 22:25:36 +0530

    Delete HomeAssignment1 directory

--- HomeAssignment1/01-PrimitiveDataTypes.js
+++
@@ -1,7 +0,0 @@
-let firstName = "John"
-let companyName = 'TestLeaf'
-let mobileNumber = 9086543216
-let isAutomation = true
-let hasPlaywright
-console.log("Values of the variables\nFirst Name: "+ firstName+"\nCompany Name: "+companyName+"\nMobileNumber: "+mobileNumber+"\nIs Automation: "+isAutomation+"\nHas Playwright: "+hasPlaywright)
-console.log("\nDataType of the variables\nFirst Name: "+ typeof(firstName)+"\nCompany Name: "+typeof(companyName)+"\nMobileNumber: "+typeof(mobileNumber)+"\nIs Automation: "+typeof(isAutomation)+"\nHas Playwright: "+typeof(hasPlaywright))
